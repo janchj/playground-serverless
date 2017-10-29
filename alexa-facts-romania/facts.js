2d92a1f47a9bc6e06ae45c0c10178dec1bd00c91
janchj <janhernandezj@gmail.com>
2017-10-29 12:13:16 +0000

    added some more facts

--- alexa-facts-romania/facts.js
+++ alexa-facts-romania/facts.js
@@ -6,7 +6,13 @@ const facts = [
   'The name “Romania” comes from the Latin word “Romanus” which means “citizen of the Roman Empire.”',
   'The Romanian Palace of Parliament in Bucharest is the second largest building in the world, next only to the Pentagon in the United States.',
   'One of the biggest and heaviest organs in Europe can be found in Brasov at the famous Black Church.',
-  'Romania is the only country in Europe where the Brown Bear still lives in the wilderness.'
+  'Romania is the only country in Europe where the Brown Bear still lives in the wilderness.',
+  'The 10-bani note issued by Romania\'s Ministry of Finance in 1917 is the smallest paper money ever printed.',
+  'Bucharest’s mass transit network is the fourth largest in Europe.',
+  'Peles Castle was the first European castle entirely lit by electrical current. The electricity was produced by the castle’s own plant.The castle’s central heating system, built in 1888, is still functional and in use today.',
+  'Timisoara was also the first European city to introduce horse-drawn trams, also in 1869.',
+  'The Danube to Black Sea canal in southeast Romania, is world’s third longest man-made navigation route, after the Suez and the Panama Canals',
+  'In 2005, the Romanian currency, the leu, dropped four of its zeroes, such that what used to be 10,000 lei became printed as just 1 lei. They also started making them out of plastic instead of paper. Legend has it that the banks were advised that they should start using more plastic (credit cards) systems, and they took it literally and started making the Lei out of plastic.'
 ];
 
 module.exports = facts;
